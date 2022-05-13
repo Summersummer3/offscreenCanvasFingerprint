f983a26b249871c24ef71b82e898988f621d9510
summer <techsum8@live.com>
2022-05-13 19:10:50 +0800

    Fix fillText is not supported by firefox

--- oscfp.js
+++ oscfp.js
@@ -17,20 +17,46 @@
 		}
 
 		const osc1 = new OffscreenCanvas(512, 512);
-		const ctx = osc1.getContext('2d');
-		var txt = 'CANVAS_FINGERPRINT';
+		let ctx;
+		try {
+			// canvas 2d is not support in Firefox old version
+			ctx = osc1.getContext('2d');
+		} catch (e) {
+			self.postMessage({
+				res: 'unsupported',
+			});
+			return; 
+		}
+		
 		ctx.textBaseline = "top";
-		ctx.font = "14px 'Arial'";
-		ctx.textBaseline = "alphabetic";
 		ctx.fillStyle = "#f60";
 		ctx.fillRect(125,1,62,20);
 
-		// TODO: Replace fillText to other functions (firefox offscreencanvas not support)
-		ctx.fillStyle = "#069";
-		ctx.fillText(txt, 2, 15);
-		ctx.fillStyle = "rgba(102, 204, 0, 0.7)";
-		ctx.fillText(txt, 4, 17);
-		
+		// Replace fillText to other functions (firefox offscreencanvas not support)
+		// Draw some lines
+		ctx.beginPath();
+		ctx.moveTo(50, 140);
+		ctx.lineTo(150, 60);
+		ctx.lineTo(250, 140);
+		ctx.closePath();
+		ctx.stroke();
+
+		// Draw a curve
+		ctx.fillStyle = "#FF0000";
+		ctx.bezierCurveTo(50, 140, 150, 60, 250, 140);
+		ctx.stroke();
+
+		// Draw some pixels
+		const imageData = ctx.createImageData(50, 50);
+		for (let i = 0; i < imageData.data.length; i += (4 * 11)) {
+			// Modify pixel data
+			imageData.data[i + 0] = 190;  // R value
+			imageData.data[i + 1] = 0;    // G value
+			imageData.data[i + 2] = 210;  // B value
+			imageData.data[i + 3] = 255;  // A value
+		}
+		ctx.putImageData(imageData, 150, 60);
+
 		// webgl
 		const osc2 = new OffscreenCanvas(256, 256);
 		const gl = osc2.getContext('webgl');
@@ -91,7 +117,7 @@
 			return h1 >>> 0;
 		}
 
-		osc2.convertToBlob().then(
+		osc1.convertToBlob().then(
 			blob => {
 				return blob.text();
 			}
